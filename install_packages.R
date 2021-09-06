ce02953524a41c6c3b2f82a3a5eaa9fc79e4fa84
Diana Simic <diana.simic@foi.unizg.hr>
2021-09-06 13:48:26 +0200

    R script for installing necessary packages.

---
+++ install_packages.R
@@ -0,0 +1,8 @@
+# Script for installing packages needed for the 
+# BIOSTAT 2001 School of Biometrics
+# Using Shiny Apps in Teaching Statistics
+# 
+
+install.packages(c("Shiny", "flexdashboard", "Rmarkdown"),
+                 dependencies = T,
+                 quiet = T)
